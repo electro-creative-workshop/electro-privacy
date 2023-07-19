92fc1396e0d0cef497a693183fe5542e2f0d4477
James de Jesus <james.dejesus@clorox.com>
2023-07-19 10:18:17 -0700

    convert concatenated lines to ES6 string template, fix minor typo

--- src/js/ot-dns-script-1.js
+++ src/js/ot-dns-script-1.js
@@ -6,60 +6,50 @@ async function OptanonWrapperLocal() {
     });
 
     if (dsIdSet == false) {
-        let otEmailHTML = '<hr />';
-        otEmailHTML += '<div id="ot-email-text" style="display: none">';
-        otEmailHTML += '<h3 style="font-size: inherit;">Behavioral Advertising Linked To Your Email Address:</h3>';
-        otEmailHTML += '<br />';
-        otEmailHTML +=
-            'You may have previously provided your email address to us. In some cases, we use email addresses or other ';
-        otEmailHTML +=
-            'non-cookie personal information to deliver behavioral advertising to consumers on third party platforms like ';
-        otEmailHTML +=
-            'Facebook and Google. To request that your personal information is not shared for these purposes, please ';
-        otEmailHTML += 'enter your email address below:';
-        otEmailHTML += '<form id="ot-email-submit" onsubmit="return false;">';
-        otEmailHTML += '<label for="ot-email">Email: ';
-        otEmailHTML += '<input type="email" id="ot-email" name="ot-email" required>';
-        otEmailHTML += '<input type="submit" id="ot-dns-submit" value="Submit">';
-        otEmailHTML += '</label></form><br>';
-        otEmailHTML +=
-            'Once you have made all of your elections, click “Save Settings” to save your settings and close the window.<br><br>';
-        otEmailHTML += '<hr />';
+        let otEmailHTML = `
+            <hr />
+            <div id="ot-email-text" style="display: none">
+                <h3 style="font-size: inherit;">Behavioral Advertising Linked To Your Email Address:</h3>
+                <br />
+                You may have previously provided your email address to us. In some cases, we use email addresses or other 
+                non-cookie personal information to deliver behavioral advertising to consumers on third party platforms like 
+                Facebook and Google. To request that your personal information is not shared for these purposes, please 
+                enter your email address below:
+                <form id="ot-email-submit" onsubmit="return false;">
+                    <label for="ot-email">Email: 
+                        <input type="email" id="ot-email" name="ot-email" required>
+                        <input type="submit" id="ot-dns-submit" value="Submit">
+                    </label>
+                </form>
+                <br />
+                Once you have made all of your elections, click “Save Settings” to save your settings and close the window.<br><br>
+                <hr />
+                <h3 style="font-size: inherit;">Deletion, Access, Or Correction Requests</h3>
+                <br />
+                If you are a U.S. consumer and would like to exercise other privacy rights, 
+                such as a deletion, access, or correction request, please visit our <a target="_blank" href="//privacyportal.onetrust.com/webform/65ca6b46-70b1-4ee1-9074-7a63e800ea4c/7baf0e2e-4724-44fe-af48-4138faca9d23">U.S. Data Subject Request</a> page.
+                <br /><br />
+                For more information about additional privacy practices and choices available to you, please visit our <a target="_blank" href="//thecloroxcompany.com/privacy/">Privacy Policy</a>.
+            </div>
+        `;
 
-        otEmailHTML += '<h3 style="font-size: inherit;">Deletion, Access, Or Correction Requests</h3>';
-
-        otEmailHTML += '<br />';
-        otEmailHTML += 'If you are a U.S. consumer and would like to exercise other privacy rights,';
-        otEmailHTML +=
-            'such as a deletion, access, or correction request, please visit our <a target="_blank" href="//privacyportal.onetrust.com/webform/65ca6b46-70b1-4ee1-9074-7a63e800ea4c/7baf0e2e-4724-44fe-af48-4138faca9d23">U.S. Data Subject Request</a> page.';
-        otEmailHTML += '<br /><br />';
-        otEmailHTML +=
-            'For more information about additional privacy practices and choices available to you, please visit our <a target="_blank" href="//thecloroxcompany.com/privacy/">Privacy Policy</a>.';
-        otEmailHTML += '</div>';
-
-        let dnsCustomText = '<div id="dns-custom-text" style="display: none">';
-        dnsCustomText += '<h3 style="font-size: inherit;">Do Not Sell or Share for Targeted Advertising</h3>';
-        dnsCustomText += '<br />';
-        dnsCustomText +=
-            'Under some state laws you have the right to opt out of the selling or sharing of your information for cross-context ';
-        dnsCustomText +=
-            'behavioral advertising and/or certain types of targeted advertising (“behavioral advertising”).<br><br>';
-        dnsCustomText +=
-            'To turn off the  behavioral advertising cookies and trackers on this website, toggle “Targeting / Advertising ';
-        dnsCustomText +=
-            'Cookies” to “off” and click “Save Settings.” If the toggle is already set to “off” - you may have already updated ';
-        dnsCustomText +=
-            'your cookie settings, or the Global Privacy Control (“GPC”) signal may be enabled in your browser.';
-        dnsCustomText += '<br><br>';
-        dnsCustomText +=
-            'In most cases, your opt-out preference will be tracked via a cookie, which means your selection is limited to the ';
-        dnsCustomText +=
-            'specific device and browser you are using during this visit to our website. If you visit this website from a ';
-        dnsCustomText +=
-            'different device or browser, change your browser settings, or if you clear your cookies, you may need to opt out again.';
-        dnsCustomText += '<br><br>';
-        dnsCustomText +=
-            'If you would like to update other cookie-related preferences visit the "Cookie Settings" link in the footer of this webpage.';
+        let dnsCustomText = `
+            <div id="dns-custom-text" style="display: none">
+                <h3 style="font-size: inherit;">Do Not Sell or Share for Targeted Advertising</h3>';
+                <br />';
+                Under some state laws you have the right to opt out of the selling or sharing of your information for cross-context 
+                behavioral advertising and/or certain types of targeted advertising (“behavioral advertising”).<br><br>
+                To turn off the  behavioral advertising cookies and trackers on this website, toggle “Targeting / Advertising 
+                Cookies” to “off” and click “Save Settings.” If the toggle is already set to “off” - you may have already updated 
+                your cookie settings, or the Global Privacy Control (“GPC”) signal may be enabled in your browser.
+                <br><br>
+                In most cases, your opt-out preference will be tracked via a cookie, which means your selection is limited to the 
+                specific device and browser you are using during this visit to our website. If you visit this website from a 
+                different device or browser, change your browser settings, or if you clear your cookies, you may need to opt out again.
+                <br><br>
+                If you would like to update other cookie-related preferences visit the "Cookie Settings" link in the footer of this webpage.
+            </div>
+        `;
 
         const otEmailForm = document.querySelectorAll('.ot-sdk-row.ot-cat-grp')[0];
         otEmailForm.insertAdjacentHTML('afterend', otEmailHTML);
